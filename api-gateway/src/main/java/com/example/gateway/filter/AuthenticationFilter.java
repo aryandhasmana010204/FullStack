684fb8a2ca75ac4831b82393fe8d9affcea62bd4
aryandhasmana010204 <aryandhasmana010204@gmail.com>
2026-06-12 15:40:39 +0530

    day 4

---
+++ api-gateway/src/main/java/com/example/gateway/filter/AuthenticationFilter.java
@@ -0,0 +1,63 @@
+package com.example.gateway.filter;
+
+import com.example.gateway.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class AuthenticationFilter implements GlobalFilter, Ordered {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+
+        final List<String> apiEndpoints = List.of("/auth/login", "/auth/register", "/auth/signup", "/eureka");
+
+        Predicate<ServerHttpRequest> isApiSecured = r -> apiEndpoints.stream()
+                .noneMatch(uri -> r.getURI().getPath().contains(uri));
+
+        if (isApiSecured.test(request)) {
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return this.onError(exchange, "Authorization header is missing in request", HttpStatus.UNAUTHORIZED);
+            }
+
+            final String token = request.getHeaders().getOrEmpty(HttpHeaders.AUTHORIZATION).get(0);
+
+            if (token != null && token.startsWith("Bearer ")) {
+                String authToken = token.substring(7);
+                if (!jwtUtil.isTokenValid(authToken)) {
+                    return this.onError(exchange, "Authorization header is invalid", HttpStatus.UNAUTHORIZED);
+                }
+            } else {
+                return this.onError(exchange, "Authorization header is invalid", HttpStatus.UNAUTHORIZED);
+            }
+        }
+        return chain.filter(exchange);
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        return response.setComplete();
+    }
+
+    @Override
+    public int getOrder() {
+        return -1; // Execute before the route execution
+    }
+}
